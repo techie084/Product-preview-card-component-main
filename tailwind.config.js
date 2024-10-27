7ea02c9b346720e062c7fa068f65c3a1f40eac41
Wisdom Matthew <wisdommatthew08@gmail.com>
2024-10-27 21:20:01 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      // primary
+      darkCyan: "#008B8B",
+      cream: "#FFFDD0",
+      // neutral
+      veryDarkBlue: "#00008B",
+      darkGrayishBlue: "#29465B",
+      white: "#ffffff",
+      green_800: "#166534",
+    },
+    fontWeight: {
+      normal: "14px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
